927934b167f65464ff793ca83ad2607f6d16a81a
Grace Nieboer <grace.nieboer@outlook.com>
2023-02-06 11:36:14 -0500

    test completed with help from Adam

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+
+function superbowlWin(someArray) {
+    const found = someArray.find(cb)
+    if (found) {
+        return found.year
+    }
+}
+
+function cb (object) {
+    if (object.result === "W")
+    return true
+}
+
+//test console.log(superbowlWin(record))
